9eee106b18338d9a1287e6773fa707061db4115c
Oscar YY <73566517+oscarlee8787@users.noreply.github.com>
2023-12-05 21:57:38 +0800

    Update streamlit_frontend.py

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import datetime
 import yfinance as yf
-import plotly.graph_objs as go
+# import plotly.graph_objs as go
 import requests
 from price_prediction.ml_logic.data import download_data
 
@@ -73,25 +73,25 @@ if coin != None:
 # Function to fetch and plot stock data
 
 
-def plot_stock(coin):
-    # Load stock data
-    data = yf.download(coin, start=datetime.date.today()-datetime.timedelta(days=90), end=datetime.date.today())
+# def plot_stock(coin):
+#     # Load stock data
+#     data = yf.download(coin, start=datetime.date.today()-datetime.timedelta(days=90), end=datetime.date.today())
 
-    # Plotting
-    fig = go.Figure()
-    fig.add_trace(go.Candlestick(x=data.index,
-                open=data['Open'],
-                high=data['High'],
-                low=data['Low'],
-                close=data['Close'],
-                name='market data'))
+#     # Plotting
+#     fig = go.Figure()
+#     fig.add_trace(go.Candlestick(x=data.index,
+#                 open=data['Open'],
+#                 high=data['High'],
+#                 low=data['Low'],
+#                 close=data['Close'],
+#                 name='market data'))
 
-    fig.update_layout(title=f'{coin}',
-                      xaxis_rangeslider_visible=False)
-    st.plotly_chart(fig, use_container_width=True)
+#     fig.update_layout(title=f'{coin}',
+#                       xaxis_rangeslider_visible=False)
+#     st.plotly_chart(fig, use_container_width=True)
 
-if coin != None:
-    plot_stock(coin)
+# if coin != None:
+#     plot_stock(coin)
 
 # converting DATE to UNIX milliseconds ------------------------------------------------------
 
