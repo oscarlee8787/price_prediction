91f6cc14271495836c709157a1d4c93cf6227cfa
Oscar Lee <oscarlee8787@gmail.com>
2023-12-06 18:44:11 +0100

    prob working

--- streamlit_frontend_Krakenapi.py
+++ streamlit_frontend_Krakenapi.py
@@ -33,9 +33,9 @@ def download_data(endtime:str, symbol:str, interval:int, limit=5):
             'interval': interval,
             'since': seconds
             }
-
     data = requests.get(url=url, params=params).json()
     result = data["result"]["XXBTZUSD"][:5]
+    # breakpoint()
 
     df = pd.DataFrame(result)
     df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'volume_weighted_avg_price',
@@ -45,6 +45,11 @@ def download_data(endtime:str, symbol:str, interval:int, limit=5):
         df[col] = df[col].astype(float)
 
     df.Date = df.Date * 1000 # turning seconds to milliseconds because kraken uses seconds but loading function was written for binance which uses millisec
+
+    client = storage.Client()
+    bucket = client.bucket('data-wrangling')
+    blob = bucket.blob(f'BTC-USD_{endtime}.csv')
+    blob.upload_from_string(df.to_csv(index=False), content_type='text/csv')
 
     return 0
 
